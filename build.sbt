9849a823487f23ce1ae69457261fc81fdae65ee6
crispy <crispywalrus@gmail.com>
2015-01-15 14:05:37 -0800

    initial commit

---
+++ build.sbt
@@ -0,0 +1,17 @@
+
+name := "socku"
+
+organization := "net.crispywalrus"
+
+scalaVersion := "2.11.5"
+
+lazy val AkkaVer = "2.3.8"
+lazy val SockoVer = "0.6.0"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % AkkaVer,
+  "org.mashupbots.socko" %% "socko-webserver" % SockoVer,
+  "org.mashupbots.socko" %% "socko-rest" % SockoVer,
+  "org.scalatest" %% "scalatest" % "2.2.3",
+  "com.typesafe.akka" %% "akka-testkit" % AkkaVer
+)
---
+++ src/main/scala/App.scala
@@ -0,0 +1,33 @@
+package net.crispywalrus.socku
+
+import org.mashupbots.socko.events._
+import org.mashupbots.socko.routes._
+import org.mashupbots.socko.webserver.{ WebServer, WebServerConfig }
+import akka.actor.{ Actor, ActorSystem, Props }
+
+class Hello extends Actor {
+  def receive = {
+    case event: HttpRequestEvent =>
+      event.response.write("Hello ")
+      context.stop(self)
+  }
+}
+
+object SockuApp extends App {
+
+  val system = ActorSystem("SockuWebapp")
+
+  val routes = Routes({
+    case GET(request) => {
+      system.actorOf(Props[Hello]) ! request
+    }
+  })
+
+  val webServer = new WebServer(WebServerConfig(), routes, system)
+  webServer.start()
+
+  Runtime.getRuntime.addShutdownHook(new Thread {
+    override def run { webServer.stop() }
+  })
+
+}
