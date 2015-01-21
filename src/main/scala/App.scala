e19ea68b540b1f65c01b063744c133fc0745212c
crispy <crispywalrus@gmail.com>
2015-01-20 21:34:42 -0800

    target stream processor make our architecture into a stream processor

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,22 +4,32 @@ import org.mashupbots.socko.events._
 import org.mashupbots.socko.routes._
 import org.mashupbots.socko.webserver.{ WebServer, WebServerConfig }
 import akka.actor.{ Actor, ActorSystem, Props }
+import akka.stream._
+import akka.stream.actor.ActorPublisher
 
-class Hello extends Actor {
+case class Something(event: HttpRequestEvent)
+
+class GetStream extends ActorPublisher[HttpRequestEvent] {
+  var count = 0
   def receive = {
-    case event: HttpRequestEvent =>
-      event.response.write("Hello ")
-      context.stop(self)
+    case msg: Something => {
+      count += 1
+      msg.event.response.write("GET %s, %s"format(msg.event.request.endPoint.path,count))
+    }
   }
 }
 
 object SockuApp extends App {
 
-  val system = ActorSystem("SockuWebapp")
+  implicit val system = ActorSystem("SockuWebapp")
+  implicit val materializer = FlowMaterializer()
+
+  val stream = system.actorOf(Props[GetStream], "stream")
 
   val routes = Routes({
-    case GET(request) => {
-      system.actorOf(Props[Hello]) ! request
+    case HttpRequest(request) => request match {
+      case GET(Path("/favicon.ico")) => request.response.write(HttpResponseStatus.NOT_FOUND)
+      case GET(_) => stream ! Something(request)
     }
   })
 
